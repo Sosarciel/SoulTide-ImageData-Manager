1da274890a165c893e555a2b3f28d1066ccda15a
zwa73 <ssfen2419@126.com>
2025-05-05 19:09:17 +0800

    add metadata

--- manager/src/BuildMetadata.ts
+++ manager/src/BuildMetadata.ts
@@ -1,5 +1,5 @@
 import { Command } from 'commander';
-import { DATASET_PATH } from './Config.schema';
+import { DATA_PATH, DATASET_PATH, PROCESSED_DIR_NAME } from './Config.schema';
 import fs from 'fs';
 import { pipe, UtilFT } from '@zwa73/utils';
 import path from 'pathe';
@@ -9,15 +9,20 @@ export const CmdBuildMetadata = (program: Command) => program
     .alias("buildmetadata")
     .description("构造trainingset的metadata.csv")
     .action(async()=>{
-        await pipe(
-            UtilFT.fileSearchGlob(DATASET_PATH, '**/processed/*/*.txt'),
-            async fps => Promise.all(fps.map(async fp =>{
-                const text = await fs.promises.readFile(fp,'utf-8');
-                return {filepath:path.relative(DATASET_PATH,fp.replace(/(.+)\.txt/,'$1.png')), text};
-            })),
-            async datas => datas.reduce((acc,cur)=>
-                `${acc}\n"${cur.filepath}","${cur.text}"`
-            ,'file_name,text'),
-            async text => fs.promises.writeFile(path.join(DATASET_PATH,'metadata.csv'),text),
-        )
+        const chars = await fs.promises.readdir(DATA_PATH);
+        chars.map(async char => {
+            const processdir = path.join(DATASET_PATH,'character',char,PROCESSED_DIR_NAME);
+            if(! await UtilFT.pathExists(processdir)) return;
+            await pipe(
+                UtilFT.fileSearchGlob(processdir, '*/*.txt'),
+                async fps => Promise.all(fps.map(async fp =>{
+                    const text = await fs.promises.readFile(fp,'utf-8');
+                    return {filepath:path.relative(processdir,fp.replace(/(.+)\.txt/,'$1.png')), text};
+                })),
+                async datas => datas.reduce((acc,cur)=>
+                    `${acc}\n"${cur.filepath}","${cur.text}"`
+                ,'file_name,text'),
+                async text => fs.promises.writeFile(path.join(processdir,'metadata.csv'),text),
+            );
+        });
 });
