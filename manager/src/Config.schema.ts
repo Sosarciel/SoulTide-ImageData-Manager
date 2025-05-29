d903255c803df9e87f99f1ad4e7b1fbc4a8bd546
zwa73 <ssfen2419@126.com>
2025-05-29 23:58:10 +0800

    fx

--- manager/src/Config.schema.ts
+++ manager/src/Config.schema.ts
@@ -36,7 +36,7 @@ export const TRAINING_SET_DIR_NAME = 'training_set';
 export const INFO_FILE_NAME = 'info.json';
 export const STYLE_FILE_NAME = 'styles.txt';
 
-export const MODEL_PATH = path.join(ROOT_PATH,'model');
+export const MODEL_PATH = path.join(ROOT_PATH,'model','character');
 export const getModelDir = (charName: string) => path.join(MODEL_PATH,charName);
 export const CURRENT_VER_DIR_NAME = 'current';
 export const ARCHIVE_VER_DIR_NAME = 'archive';
