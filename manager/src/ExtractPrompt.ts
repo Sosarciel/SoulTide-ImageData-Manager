4e5a3b9bf3b9496a127843a251234e24dc7c4d1d
zwa73 <ssfen2419@126.com>
2025-07-02 15:56:59 +0800

    fx

--- manager/src/ExtractPrompt.ts
+++ manager/src/ExtractPrompt.ts
@@ -7,8 +7,9 @@ export const CmdExtractPrompt = (program: Command) => program
     .alias("extractprompt")
     .description("匹配并排除style")
     .argument("<input>", "输入prompt 逗号分隔",(str)=>str.replace(/[\r\n]/g,'').replace(/_/g,' ').split(','))
-    .option("-e, --exclude <list>", `排除列表 *为全部 逗号分隔 允许值为:${Object.keys(getPatternsCategory()).join('|')}`,parseStrlist)
-    .option("-r, --reserve <list>", `保留列表 *为全部 逗号分隔 允许值为:${Object.keys(getPatternsCategory()).join('|')}`,parseStrlist)
+    .option("-e, --exclude <list>", `排除列表 *为全部 逗号分隔 默认无 允许值为:${Object.keys(getPatternsCategory()).join('|')}`,parseStrlist)
+    .option("-r, --reserve <list>", `保留列表 *为全部 逗号分隔 与exclude冲突时排除 默认全部保留 允许值为:${Object.keys(getPatternsCategory()).join('|')}`,parseStrlist)
+    .option("-m, --must <list>", `优先保留列表 *为全部 逗号分隔 与exclude冲突时保留 默认无 允许值为:${Object.keys(getPatternsCategory()).join('|')}`,parseStrlist)
     .action(async(input:string[],opt?:ExtractPromptOpt)=>{
         const inputmap = getPromptCountMap(input);
         if(opt?.exclude?.[0]=="*") opt.exclude = (await getPatternsCategory()).map(v=>v.name);
