89411c69981899e7dd52283992a5693cf4b47d59
zwa73 <ssfen2419@126.com>
2025-07-21 06:43:29 +0800

    fx

--- manager/src/ExtractPrompt.ts
+++ manager/src/ExtractPrompt.ts
@@ -11,7 +11,7 @@ export const CmdExtractPrompt = (program: Command) => program
     .command("Extract-Prompt")
     .alias("extractprompt")
     .description("匹配并排除style")
-    .argument("<input>", "输入prompt",str=>str.replace(/[\r\n]/g,'').replace(/_/g,' ').split(','))
+    .argument("<input>", "输入prompt",str=>str.replace(/[\r\n]/g,'').replace(/_/g,' ').replace(/\\(\(|\))/g,'$1').split(','))
     .option("-i, --include <list>", `包含列表 与exclude冲突时排除 默认包含全部 允许值为:${Object.keys(getPatternsCategory()).join('|')}`,parseStrlist)
     .option("-e, --exclude <list>", `排除列表 默认无 允许值为:${Object.keys(getPatternsCategory()).join('|')}`,parseStrlist)
     .option("-r, --reserve <list>", `优先保留列表 与exclude冲突时保留 默认无 允许值为:${Object.keys(getPatternsCategory()).join('|')}`,parseStrlist)
